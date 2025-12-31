1bc000988946edea9f6e330a8a91af1bc513cd0f
708Tia <tmabry2@gmail.com>
2025-12-30 21:16:46 -0600

    Add files via upload

---
+++ PythonProject1/coffee shop.py
@@ -0,0 +1,11 @@
+#hope this works
+#print("Hello,Welcome to Tia Coffee Shop!!!!!!!!!!!!!")
+name = input ("what is your name?\n")
+print("Hello " + name + ",thank you so much for coming in today")
+
+menu = "black coffee, Espresso, Latte, Cappucino"
+print(name + ", what would you like from our menu today?  Here is what we are serving.\n" +menu)
+
+order = input()
+
+print("sounds good " + name + ", we'll have that " + order + " right up for you")
